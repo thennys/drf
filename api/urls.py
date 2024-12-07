3d6254a34b46de731a271d15f90d6a94c6749379
thennys <thennyswhyte@gmail.com>
2024-12-06 14:26:23 -1200

    Permission classes added

--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,7 @@ from api import views
 
 
 urlpatterns = [
-    path('todos/', views.Todolist.as_view()),
+    path('todos/', views.TodoList.as_view()),
     path('todos/<int:pk>', views.TodoDetail.as_view()),
     path('users/', views.UserList.as_view()),
     path('users/<int:pk>/', views.UserDetail.as_view()),
