a6cf3decfc642c7eb0d9ab35ca7cdfa9f3da318a
thennys <thennyswhyte@gmail.com>
2024-12-11 23:17:46 -1200

    refactored with ViewSets

--- api/urls.py
+++ api/urls.py
@@ -1,14 +1,31 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from api import views
+from api.views import TodoViewSet, UserViewSet, api_root
 
+todo_list = TodoViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
 
+todo_detail = TodoViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+
+user_list = UserViewSet.as_view({
+    'get': 'list'
+})
+user_detail = UserViewSet.as_view({
+    'get': 'retrieve'
+})
 urlpatterns = [
-    path('api/', views.api_root),
-    path('', views.TodoList.as_view(), name='todo-list'),
-    path('<int:pk>', views.TodoDetail.as_view(), name='todo-detail'),
-    path('users/', views.UserList.as_view(), name='user-list'),
-    path('users/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
+    path('api/', api_root),
+    path('', todo_list, name='todo-list'),
+    path('<int:pk>', todo_detail, name='todo-detail'),
+    path('users/', user_list, name='user-list'),
+    path('users/<int:pk>/', user_detail, name='user-detail'),
 
 ]
 
--- api/views.py
+++ api/views.py
@@ -1,12 +1,15 @@
 from api.models import Todo
 from api.serializers import TodoSerializer, UserSerializer
 from api.permissions import IsOwnerOrReadOnly
+from rest_framework import viewsets
 
 from rest_framework import generics
-from rest_framework import permissions
+from rest_framework import permissions, renderers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
+
+from rest_framework.decorators import action
 
 from django.contrib.auth.models import User
 
@@ -19,24 +22,18 @@ def api_root(request, format=None):
     })
 
 
-class TodoList(generics.ListCreateAPIView):
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class TodoViewSet(viewsets.ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class=TodoSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    
+    @action(detail=True)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
-
-class TodoDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Todo.objects.all()
-    serializer_class = TodoSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
-
-class UserList(generics.ListAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-class UserDetail(generics.RetrieveAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
